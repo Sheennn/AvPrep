ac500cc60c86fd58beb6ce92cdc3582c0d7c20e9
Eoin Mitchell <eoinmitchell19@gmail.com>
2025-08-09 19:07:58 +0100

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -41,7 +41,7 @@ const Header = () => {
           {/* Logo */}
           <div className="flex items-center">
             <Link to="/" className="flex items-center">
-              <img src="/logo.png" alt="Leading Edge" title="Leading Edge" className="h-12 md:h-14 w-auto" />
+              <img src="https://media.discordapp.net/attachments/630403344342712365/1403800952653221909/logo.png?ex=6898df04&is=68978d84&hm=5f3c8d4b38ea71d6c4eb37c042e4c45e0a6e7d478312c91a674bd82b144ebfd9&=&format=webp&quality=lossless&width=922&height=274" alt="Leading Edge" title="Leading Edge" className="h-12 md:h-14 w-auto" />
             </Link>
             
             {/* Show current subject and mode when in test */}
